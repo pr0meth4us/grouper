cd0c9d70afffa492b2248597062a099aae5180ee
pr0meth4us <phearaniron.soeung@gmail.com>
2023-12-08 15:13:07 +0700

    use id to shuffle instead

--- src/main/java/dev/prometheus/grouping/admin/AdminController.java
+++ src/main/java/dev/prometheus/grouping/admin/AdminController.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
