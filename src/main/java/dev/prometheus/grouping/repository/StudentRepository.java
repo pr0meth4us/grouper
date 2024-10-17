c8847a0afe886974dfab5969dcf2c20d0c788dee
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-17 16:02:27 +0700

    feat: remodulize frontend

--- src/main/java/dev/prometheus/grouping/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.prometheus.grouping.repository;
-
-import dev.prometheus.grouping.model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentRepository extends MongoRepository<Student, String> {
-    boolean existsByName(String name);
-}
