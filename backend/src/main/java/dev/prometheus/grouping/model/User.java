7059a60ba577452342888dd94393d489c66fc587
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-21 20:17:40 +0700

    feat: user model

---
+++ backend/src/main/java/dev/prometheus/grouping/model/User.java
@@ -0,0 +1,15 @@
+package dev.prometheus.grouping.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String email;
+    private String password;
+    private String[] list;
+}
