5918f9316aa10ddb71e52a00a20b24f5b0d86620
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-21 18:39:12 +0700

    fix: leave id creation to mongo

--- backend/src/main/java/dev/prometheus/grouping/model/Student.java
+++ backend/src/main/java/dev/prometheus/grouping/model/Student.java
@@ -4,12 +4,10 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.UUID;
-
 @Data
 @Document(collection = "students")
 public class Student {
     @Id
-    private String id = UUID.randomUUID().toString();
+    private String id;
     private String name;
 }
