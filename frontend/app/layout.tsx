25a56f1dedec7bf63766e6fa8b8566bd7e4c7944
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-11-01 09:31:49 +0700

    footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -46,11 +46,11 @@ export default function RootLayout({
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+                href="https://github.com/pr0meth4us"
                 title="nextui.org homepage"
               >
                 <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
+                <p className="text-primary">Pr0meth4us</p>
               </Link>
             </footer>
           </div>
